d8b80fef5b1d12299188352662e8f238b671ea14
code <carlos.oliveira.dev.junior@gmail.com>
2025-05-11 16:49:00 -0300

    ctrl shift i para formatar o codigo

--- src/app/util/TerminalUtil.ts
+++ src/app/util/TerminalUtil.ts
@@ -15,10 +15,7 @@ export default class TerminalUtil {
         terminal.yellow(chave).green(valor).white("\n")
     }
 
-    static async campoRequerido(
-        label: string,
-        valorPadrao: string = ""
-    ): Promise<string> {
+    static async campoRequerido(label: string, valorPadrao: string = ""): Promise<string> {
         terminal.yellow(`\n${label}`)
         const valor = await terminal.inputField({
             default: valorPadrao,
@@ -27,55 +24,33 @@ export default class TerminalUtil {
         return TerminalUtil.campoRequerido(label)
     }
 
-    static async menu(
-        opcoes: string[]
-    ): Promise<[number, string]> {
-        const resposta = await terminal.singleColumnMenu(
-            opcoes
-        ).promise
-        return [
-            resposta.selectedIndex,
-            resposta.selectedText,
-        ]
+    static async menu(opcoes: string[]): Promise<[number, string]> {
+        const resposta = await terminal.singleColumnMenu(opcoes).promise
+        return [resposta.selectedIndex, resposta.selectedText]
     }
 
-    static async selecao(
-        texto: string,
-        opcoes: string[]
-    ): Promise<[number, string]> {
+    static async selecao(texto: string, opcoes: string[]): Promise<[number, string]> {
         terminal.yellow(`\n${texto}`)
-        const resposta = await terminal.singleLineMenu(
-            opcoes
-        ).promise
-        return [
-            resposta.selectedIndex,
-            resposta.selectedText,
-        ]
+        const resposta = await terminal.singleLineMenu(opcoes).promise
+        return [resposta.selectedIndex, resposta.selectedText]
     }
 
-    static async confirmacao(
-        texto: string
-    ): Promise<boolean> {
+    static async confirmacao(texto: string): Promise<boolean> {
         terminal.yellow(`\n${texto}`)
-        const resposta = await terminal.singleLineMenu([
-            "Sim",
-            "Não",
-        ]).promise
+        const resposta = await terminal.singleLineMenu(["Sim", "Não"]).promise
         return resposta.selectedIndex === 0
     }
 
     static async esperarEnter(): Promise<void> {
-        terminal.white(
-            "\nPressione ENTER para continuar..."
-        )
+        terminal.white("\nPressione ENTER para continuar...")
         await terminal.inputField({ echo: false }).promise
     }
 
     static async sucesso(texto: string, novaLinha: boolean = true) {
-        terminal.green((novaLinha ? '\n' : '') + texto)
+        terminal.green((novaLinha ? "\n" : "") + texto)
     }
 
     static async erro(texto: string, novaLinha: boolean = true) {
-        terminal.red((novaLinha ? '\n' : '') + texto)
+        terminal.red((novaLinha ? "\n" : "") + texto)
     }
 }
